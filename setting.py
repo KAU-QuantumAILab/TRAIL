03439a294fdba7ca8e3177f62cafd2443b78e5c1
hamsss <gka304@naver.com>
2024-08-17 00:28:11 +0900

    first commit

---
+++ setting.py
@@ -0,0 +1,49 @@
+import argparse
+
+class Setting():
+    
+    def __init__(self):
+        pass
+    
+    def init_state(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--cuda', type=int, default = 0,
+                            help= 'cuda number')        
+        parser.add_argument('--epochs', type=int, default=1200,
+                            help='Number of epochs to train.')
+        parser.add_argument('--lr', type=float, default=5e-4,
+                            help='Initial learning rate.')
+        parser.add_argument('--weight_decay', type=float, default=5e-4,
+                            help='Weight decay (L2 loss on parameters).')
+        parser.add_argument('--dropout', type=float, default=0.6,
+                            help='Dropout rate (1 - keep probability).')
+        parser.add_argument('--layer', type=int, default = 128,
+                            help= 'Number of hidden layer')
+        parser.add_argument('--hidden', type=int, default=128,
+                            help='Number of hidden layer dimension.')
+        parser.add_argument('--leaky_slop', type=float, default = 0.3,
+                            help= 'Leaky ReLU negative slope')        
+        parser.add_argument('--alpha', type=float, default = 0.35,
+                            help= 'proportion of vector')
+        parser.add_argument('--momentum', type=float, default = 0.05,
+                            help= 'batch norm momentum')        
+        parser.add_argument('--seed', type=float, default = 42,
+                            help= 'set the seed')
+        parser.add_argument('--model', type=str, default = 'GCN',
+                            help= 'graph model GCN/GraphSage/SGC')
+        parser.add_argument('--dataset', type=str, default = 'texas',
+                            help= 'set the dataset')
+
+        args = parser.parse_args()
+        
+        return args
+    
+'''
+now available datasets
+Cora, Citeseer, Pubmed, CS, Physics, Computers, Photo, Wiki, Cornell, Texas, 
+Wisconsin, Chameleon, Squirrel, Actor, DBLP, CoraFull, Flickr, Facebook
+'''
+'''
+seed
+42, 200 ~ 1500
+'''
---
+++ train.py
@@ -0,0 +1,48 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+import time
+import torch
+import torch.optim as optim
+from pygcn.utils import load_data, set_seed, train, test
+from pygcn.models import AE_GNN
+from pygcn.setting import Setting
+
+
+def main(args):
+    # load data
+    adj, features, labels, idx_train, idx_val = load_data(args.dataset)
+    
+    # model
+    model = AE_GNN(nfeat=features.shape[1], nhid=args.hidden, nclass=labels.max().item() + 1,
+                    dropout=args.dropout, num_layer=args.layer, alpha = args.alpha, 
+                    model=args.model, leaky_slop = args.leaky_slop, momentum=args.momentum)
+
+    # optimizer
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+
+    model = model.to(device)
+    features = features.to(device)
+    adj = adj.to(device)
+    labels = labels.to(device)
+    idx_train = idx_train.to(device)
+    idx_val = idx_val.to(device)
+    # idx_test = idx_test.to(device)
+
+    t_total = time.time()
+    # Train model
+    maxacc, dis_ratio = train(args.epochs, model, optimizer, features, 
+                              adj, labels, idx_train, idx_val, )
+    take_time = time.time() - t_total
+
+    print(f"Time: {take_time:.2f}s, Max validation acc: {100 * maxacc:.2f}%, DGR: {dis_ratio:.4f}")
+
+    # acc_test, loss_test = test(model, features, adj, idx_test, labels)
+
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print('Device:', device)
+    
+    args = Setting().init_state()
+    set_seed(args.seed, args.cuda)
+    main(args)
